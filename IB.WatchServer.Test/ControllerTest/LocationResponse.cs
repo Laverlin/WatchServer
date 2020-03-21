2d07e2f0b0f22c715fd5a3f489e9763c66a11d28
IB <i6162746@hotmail.com>
2020-03-21 15:38:17 +0300

    Add location test

---
+++ IB.WatchServer.Test/ControllerTest/LocationResponse.cs
@@ -0,0 +1,16 @@
+﻿
+using IB.WatchServer.Service.Entity;
+
+namespace IB.WatchServer.Test.ControllerTest
+{
+    /// <summary>
+    /// Represents the location description
+    /// </summary>
+    public class LocationResponse : BaseApiResponse
+    {
+        /// <summary>
+        /// The Name of the plase 
+        /// </summary>
+        public string CityName { get; set; }
+    }
+}
---
+++ IB.WatchServer.Test/ControllerTest/YAFaceControllerTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json;
+using IB.WatchServer.Service.Controllers;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IB.WatchServer.Test.ControllerTest
+{
+    [TestClass]
+    public class YAFaceControllerTest
+    {
+
+
+        [TestMethod]
+        public void LocationShouldReturnUpgradeMessage()
+        {
+            // Arrange
+            //
+            var controller = new YAFaceController(null, null, null, null, null);
+            
+            var expected = new LocationResponse {CityName = "Update required."};
+            var expectedJson = JsonSerializer.Serialize(expected);
+
+            // Act
+            //
+            var result = controller.Location();
+            var resultJson = JsonSerializer.Serialize(result.Value);
+
+            // Arrange
+            //
+            Assert.AreEqual(expected.CityName, result.Value.CityName);
+            Assert.AreEqual(expectedJson, resultJson);
+        }
+    }
+}
