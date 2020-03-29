971deebdbf6f8f8e898a894bdb5d75fde7d8915b
IB <i6162746@hotmail.com>
2020-03-29 18:34:04 +0300

    Add exchange rate cache test

---
+++ IB.WatchServer.XUnitTest/UnitTests/WebRequestProviderTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using App.Metrics;
+using IB.WatchServer.Service.Entity.WatchFace;
+using IB.WatchServer.Service.Service;
+using Moq;
+using Moq.Contrib.HttpClient;
+using Xunit;
+
+namespace IB.WatchServer.XUnitTest.UnitTests
+{
+    public class WebRequestProviderTest
+    {
+        [Fact]
+        public async void ExchangeRateWithNewCurrencyPairShouldMakeRequest()
+        {
+            // Arrange
+            //
+ //           var handler = new Mock<HttpMessageHandler>();
+ //           handler.SetupAnyRequest()
+ //               .ReturnsResponse(HttpStatusCode.OK, "{\"USD_PHP\": {\r\n\"val\": 51.440375\r\n}}")
+ //               .Verifiable();
+
+            var metricsMock = new Mock<IMetrics>();
+
+ //           var httpClientFactoryMock = handler.CreateClientFactory();
+            var webRequestProvider = new WebRequestsProvider(null, null, null, metricsMock.Object, null);
+
+            string actualBase="";
+            string actualTarget="";
+            int callCount = 0;
+
+            // Act
+            //
+            await webRequestProvider.RequestCacheExchangeRate("USD", "PHP", 
+                (b, t) =>
+                {
+                    actualBase = b;
+                    actualTarget = t;
+                    callCount++;
+                    return Task.FromResult(new ExchangeRateInfo {ExchangeRate = (decimal) 51.440375});
+                });
+
+            // Assert
+            //
+            //handler.VerifyAnyRequest(Times.Exactly(1));
+            Assert.Equal("USD", actualBase);
+            Assert.Equal("PHP", actualTarget);
+            Assert.Equal(1, callCount);
+        }
+    }
+}
