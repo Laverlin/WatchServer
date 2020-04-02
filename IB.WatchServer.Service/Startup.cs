b6c7b70e7ed0a060a74907dbf83a733a1ec89423
IB <i6162746@hotmail.com>
2020-04-02 12:50:55 +0300

    Move HealthCheck report formatter to separate class

--- IB.WatchServer.Service/Startup.cs
+++ IB.WatchServer.Service/Startup.cs
@@ -175,54 +175,11 @@ namespace IB.WatchServer.Service
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
-                endpoints.MapHealthChecks("/health", new HealthCheckOptions {ResponseWriter = WriteHealthResultResponse});
+                endpoints.MapHealthChecks("/health",
+                    new HealthCheckOptions {ResponseWriter = HealthCheckExtensions.WriteHealthResultResponse});
             });
             app.UseMetricsEndpoint();
         }
 
-        /// <summary>
-        /// Custom JSON output for HealthReport
-        /// </summary>
-        private static Task WriteHealthResultResponse(HttpContext context, HealthReport result)
-        {
-            context.Response.ContentType = "application/json; charset=utf-8";
-
-            var options = new JsonWriterOptions
-            {
-                Indented = true
-            };
-
-            using var stream = new MemoryStream();
-            using (var writer = new Utf8JsonWriter(stream, options))
-            {
-                writer.WriteStartObject();
-                writer.WriteString("serverVersion", SolutionInfo.Version);
-                writer.WriteString("status", result.Status.ToString());
-                writer.WriteString("totalDuration", result.TotalDuration.ToString());
-                writer.WriteStartObject("results");
-                foreach (var entry in result.Entries)
-                {
-                    writer.WriteStartObject(entry.Key);
-                    writer.WriteString("status", entry.Value.Status.ToString());
-                    writer.WriteString("description", entry.Value.Description);
-                    writer.WriteStartObject("data");
-                    foreach (var item in entry.Value.Data)
-                    {
-                        writer.WritePropertyName(item.Key);
-                        JsonSerializer.Serialize(writer, item.Value, item.Value?.GetType() ?? typeof(object));
-                    }
-
-                    writer.WriteEndObject();
-                    writer.WriteEndObject();
-                }
-
-                writer.WriteEndObject();
-                writer.WriteEndObject();
-            }
-
-            var json = Encoding.UTF8.GetString(stream.ToArray());
-
-            return context.Response.WriteAsync(json);
-        }
     }
 }
---
+++ IB.WatchServer.XUnitTest/UnitTests/HealthCheckTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Text;
+using IB.WatchServer.Service.Infrastructure;
+using IB.WatchServer.Service.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Xunit;
+
+namespace IB.WatchServer.XUnitTest.UnitTests
+{
+    public class HealthCheckTest
+    {
+        [Fact]
+        public void HealthCheckShouldReturnHealthJson()
+        {
+            // Arrange
+            //
+            var expectedJson =
+                "{\r\n  \"serverVersion\": \"" + SolutionInfo.Version + "\",\r\n  \"status\": \"Healthy\",\r\n  \"totalDuration\": \"00:00:01.1490236\",\r\n  \"results\": {\r\n    \"database\": {\r\n      \"status\": \"Healthy\",\r\n      \"description\": null,\r\n      \"data\": {}\r\n    },\r\n    \"location\": {\r\n      \"status\": \"Healthy\",\r\n      \"description\": null,\r\n      \"data\": {}\r\n    }\r\n  }\r\n}";
+            var reports = new Dictionary<string, HealthReportEntry> {
+            {
+                "database", 
+                new HealthReportEntry(HealthStatus.Healthy, null, TimeSpan.FromSeconds(1), null, null)
+            },
+            {
+                "location", 
+                new HealthReportEntry(HealthStatus.Healthy, null, TimeSpan.FromSeconds(1), null, null)
+            }
+            };
+
+            var report = new HealthReport(
+                new ReadOnlyDictionary<string, HealthReportEntry>(reports), TimeSpan.FromMilliseconds(1149.0236));
+
+            HttpContext context = new DefaultHttpContext();
+            context.Response.Body = new MemoryStream();
+            
+
+            // Act
+            //
+            HealthCheckExtensions.WriteHealthResultResponse(context, report);
+
+            var code = context.Response.StatusCode;
+            context.Response.Body.Position = 0;
+            var result = new StreamReader(context.Response.Body, Encoding.UTF8).ReadToEnd();
+
+            //Assert
+            //
+            Assert.Equal(200, code);
+            Assert.Equal(expectedJson, result);
+
+        }
+    }
+}
