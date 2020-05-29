1c34e10704ebb6d5943341fb0566002cf905b638
IB <i6162746@hotmail.com>
2020-05-29 22:54:48 +0300

    Config for Kafka

--- IB.WatchServer.RequestCollector/CollectorFunction.cs
+++ IB.WatchServer.RequestCollector/CollectorFunction.cs
@@ -1,4 +1,6 @@
+
 using System;
+using Confluent.Kafka;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 
@@ -16,13 +18,47 @@ namespace IB.WatchServer.RequestCollector
             _collectorSettings = collectorSettings;
         }
 
+        /// <summary>
+        /// Execute function
+        /// </summary>
+        /// <param name="timerInfo">timer info</param>
+        /// <param name="logger">logger</param>
+        /// <param name="context">context</param>
         [FunctionName("CollectorFunction")]
         public void Run([TimerTrigger("*/5 * * * * *")]TimerInfo timerInfo, ILogger logger, ExecutionContext context)
         {
+            var consumerConfig = new ConsumerConfig
+            {
+                BootstrapServers = _collectorSettings.KafkaServer,
+                GroupId = _collectorSettings.KafkaConsumerGroup,
+                AutoOffsetReset = AutoOffsetReset.Earliest,
+                EnablePartitionEof = true
+            };
 
-            logger.LogDebug($"C# Timer trigger function executed at: {DateTime.Now}");
-            logger.LogInformation($"Value SQL: {_collectorSettings.SqlConnection}");
-            logger.LogInformation($"Value Kafka: {_collectorSettings.KafkaConnection}");
+            using var consumer = new ConsumerBuilder<Ignore, string>(consumerConfig).Build();
+
+            consumer.Subscribe(_collectorSettings.KafkaTopic);
+
+            try
+            {
+                while (true)
+                {
+                    var payload = consumer.Consume();
+                    if (payload.IsPartitionEOF)
+                        break;
+
+                    var message = payload?.Message.Value;
+                    logger.LogInformation($"message: {message}");
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.LogDebug(ex, "");
+            }
+            finally
+            {
+                consumer.Close();
+            }
         }
     }
 }
--- IB.WatchServer.RequestCollector/CollectorSettings.cs
+++ IB.WatchServer.RequestCollector/CollectorSettings.cs
@@ -7,6 +7,20 @@ namespace IB.WatchServer.RequestCollector
     public class CollectorSettings
     {
         public string SqlConnection { get; set; }
-        public string KafkaConnection { get; set; }
+        
+        /// <summary>
+        /// Kafka Server Url and port
+        /// </summary>
+        public string KafkaServer { get; set; }
+
+        /// <summary>
+        /// Queue topic 
+        /// </summary>
+        public string KafkaTopic { get; set; }
+
+        /// <summary>
+        /// Consumer group
+        /// </summary>
+        public string KafkaConsumerGroup { get; set; }
     }
 }
